937dd4676170c0cad437af4e5e4b61a4f06f0326
Andrew Heins <andrew.heins@gmail.com>
2015-12-17 23:40:41 -0500

    New Uploads style

---
+++ wp-config-local-sample.php
@@ -0,0 +1,26 @@
+<?php 
+
+/** Environment Variable - Allows for server-specific code **/
+define('ENV', 'DEVELOPMENT');
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', true);
