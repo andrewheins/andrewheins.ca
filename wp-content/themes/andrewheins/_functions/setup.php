08d3500e274842fa5d4917b657004cb357a4d4d9
Andrew Heins <andrew.heins@gmail.com>
2015-12-30 09:46:39 -0500

    Fixes jQuery on HTTPS

--- wp-content/themes/andrewheins/_functions/setup.php
+++ wp-content/themes/andrewheins/_functions/setup.php
@@ -157,6 +157,14 @@
    Scripts and Styles
    ========================================================================== */
    
+   function jquery_cdn() {
+	   wp_deregister_script('jquery');
+	   wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', false, null);
+	   wp_enqueue_script('jquery');
+	}
+	
+	add_action('wp_enqueue_scripts', 'jquery_cdn');
+
 
 	/*
 	 * Load All Scripts via enqueue
--- wp-content/themes/andrewheins/footer.php
+++ wp-content/themes/andrewheins/footer.php
@@ -11,7 +11,6 @@
 
 </div>
 	
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<?php wp_footer(); ?>
     
 </body>
