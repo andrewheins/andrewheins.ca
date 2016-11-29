67213c81299b2deb73a739ad8033d28691c79b42
Andrew Heins <andrew.heins@gmail.com>
2016-11-29 13:40:50 -0500

    unlock from specific Cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+# lock '3.4.0'
 
 set :application, 'andrewheins'
 set :repo_url, 'git@github.com:andrewheins/andrewheins.ca.git'
